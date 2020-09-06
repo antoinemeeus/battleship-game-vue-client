fce9d3b0069f5f4e2e511f860e12e61b37defd30
Antoine Meeus <antoine.meeus@mytheresa.com>
2020-09-06 17:10:15 +0200

    INTERNAL: lint main and router js files

--- src/main.js
+++ src/main.js
@@ -10,16 +10,17 @@ import "@fortawesome/fontawesome-free/css/all.css"; // Ensure you are using css-
 
 // Helpers
 import moment from "moment";
+
 Vue.prototype.moment = moment;
 
 Vue.config.productionTip = false;
 
 Vue.use(Vuetify, {
-  iconfont: "fa"
+    iconfont: "fa"
 });
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
